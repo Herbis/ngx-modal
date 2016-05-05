50629206f054df336404869b6afe923528135eb1
Gary Taylor <gravity.addiction@gmail.com>
2016-05-05 09:20:41 -0700

    updated to angular2.rc.1

--- src/Modal.ts
+++ src/Modal.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter, ElementRef, ViewChild} from "angular2/core";
+import {Component, Input, Output, EventEmitter, ElementRef, ViewChild} from "@angular/core";
 
 @Component({
     selector: "modal",
