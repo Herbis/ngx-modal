3f83aaef674d4e02e768190c20a155685f5fc07c
Umed Khudoiberdiev <zarrhost@gmail.com>
2016-07-12 15:54:42 +0500

    removed tabindex because of bug

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import {Component, Input, Output, EventEmitter, ElementRef, ViewChild} from "@an
 @Component({
     selector: "modal",
     template: `
-<div class="modal" tabindex="-1" role="dialog"
+<div class="modal" role="dialog"
      #modalRoot
      (keydown.esc)="closeOnEscape ? close() : 0"
      [ngClass]="{ in: isOpened, fade: isOpened }"
      [ngStyle]="{ display: isOpened ? 'block' : 'none' }"
      (click)="closeOnOutsideClick ? close() : 0">
     <div [class]="'modal-dialog ' + modalClass" (click)="preventClosing($event)">
-        <div class="modal-content" tabindex="0" *ngIf="isOpened">
+        <div class="modal-content" *ngIf="isOpened">
             <div class="modal-header">
                 <button *ngIf="!hideCloseButton" type="button" class="close" data-dismiss="modal" [attr.aria-label]="cancelButtonLabel || 'Close'" (click)="close()"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" *ngIf="title">{{ title }}</h4>
