144c0c48de776a44a0d15e8941988c61525543e8
Umed Khudoiberdiev <zarrhost@gmail.com>
2016-07-14 19:06:15 +0500

    fixed source maps issue and close on esc issue

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import {Component, Input, Output, EventEmitter, ElementRef, ViewChild} from "@an
 @Component({
     selector: "modal",
     template: `
-<div class="modal" role="dialog"
+<div class="modal" 
+     tabindex="-1"
+     role="dialog"
      #modalRoot
      (keydown.esc)="closeOnEscape ? close() : 0"
      [ngClass]="{ in: isOpened, fade: isOpened }"
      [ngStyle]="{ display: isOpened ? 'block' : 'none' }"
      (click)="closeOnOutsideClick ? close() : 0">
     <div [class]="'modal-dialog ' + modalClass" (click)="preventClosing($event)">
-        <div class="modal-content" *ngIf="isOpened">
+        <div class="modal-content" tabindex="0" *ngIf="isOpened">
             <div class="modal-header">
                 <button *ngIf="!hideCloseButton" type="button" class="close" data-dismiss="modal" [attr.aria-label]="cancelButtonLabel || 'Close'" (click)="close()"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" *ngIf="title">{{ title }}</h4>
@@ -93,6 +95,9 @@ export class Modal {
     // -------------------------------------------------------------------------
 
     open(...args: any[]) {
+        if (this.isOpened)
+            return;
+        
         this.isOpened = true;
         this.onOpen.emit(args);
         document.body.appendChild(this.backdropElement);
@@ -100,6 +105,9 @@ export class Modal {
     }
 
     close(...args: any[]) {
+        if (!this.isOpened)
+            return;
+
         this.isOpened = false;
         this.onClose.emit(args);
         document.body.removeChild(this.backdropElement);
